ec53ce77abf63379f09a35b6eab533629a2b0cbd
Benjamin Guibert <contact@bguibert.com>
2024-04-06 16:23:59 +0200

    feat(main): update rules

--- index.cjs
+++ index.cjs
@@ -2,14 +2,14 @@
 const configuration = {
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
-  rules: {
-    "no-console": "warn",
-  },
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended-type-checked",
     "plugin:@typescript-eslint/stylistic-type-checked",
   ],
+  rules: {
+    "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+  },
 };
 
 module.exports = configuration;
